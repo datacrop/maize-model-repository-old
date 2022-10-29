b4fb616d3bc9f0aa195bea24e0bb19783c2c9b4c
Angela-Maria Despotopoulou <a.despotopoulou@gmail.com>
2022-10-30 02:48:25 +0300

    Created Java Spring Boot Project structure.

---
+++ model-repository-server/api/src/main/java/eu/datacrop/maize/model_repository/api/ModelRepositoryServerApplication.java
@@ -0,0 +1,13 @@
+package eu.datacrop.maize.model_repository.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelRepositoryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModelRepositoryServerApplication.class, args);
+    }
+
+}
