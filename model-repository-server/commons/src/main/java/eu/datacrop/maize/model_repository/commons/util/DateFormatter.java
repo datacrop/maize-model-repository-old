d7a033336736def38744713a5fe76ef53d8ad666
Angela-Maria Despotopoulou <angelamariadespotopoulou@users.noreply.github.com>
2022-10-30 21:49:58 +0200

    Added javadoc documentation to recently created "commons" classes.

--- model-repository-server/commons/src/main/java/eu/datacrop/maize/model_repository/commons/util/DateFormatter.java
+++ model-repository-server/commons/src/main/java/eu/datacrop/maize/model_repository/commons/util/DateFormatter.java
@@ -2,10 +2,22 @@ package eu.datacrop.maize.model_repository.commons.util;
 
 import java.time.format.DateTimeFormatter;
 
+/**********************************************************************************************************************
+ * This class offers the functionality of a global DateTime Formatter.
+ *
+ * @author Angela-Maria Despotopoulou
+ * @since version 0.3.0
+ *********************************************************************************************************************/
 public class DateFormatter {
 
+    /******************************************************************************************************************
+     * Definition of the global DateTime Formatter.
+     *****************************************************************************************************************/
     public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
 
+    /******************************************************************************************************************
+     * Constructor of the DateTimeFormatter class (private to disallow instantiation).
+     *****************************************************************************************************************/
     private DateFormatter() {
         throw new IllegalStateException("Utility classes should not be instantiated.");
     }
