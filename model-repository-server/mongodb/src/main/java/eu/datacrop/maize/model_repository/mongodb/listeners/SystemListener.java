aefd150cdcd30954ddc91facb17d576fc1e21abd
Angela-Maria Despotopoulou <a.despotopoulou@gmail.com>
2022-10-31 03:40:16 +0200

    Implemented mongodb.model.System and mongodb.listeners.AbstractMongoEventListener.

--- model-repository-server/mongodb/src/main/java/eu/datacrop/maize/model_repository/mongodb/listeners/SystemListener.java
+++ model-repository-server/mongodb/src/main/java/eu/datacrop/maize/model_repository/mongodb/listeners/SystemListener.java
@@ -1,4 +1,35 @@
 package eu.datacrop.maize.model_repository.mongodb.listeners;
 
-public class SystemListener {
+import eu.datacrop.maize.model_repository.mongodb.model.System;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
+import org.springframework.data.mongodb.core.mapping.event.AfterDeleteEvent;
+import org.springframework.data.mongodb.core.mapping.event.AfterSaveEvent;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class SystemListener extends AbstractMongoEventListener<System> {
+
+    @Override
+    public void onAfterSave(AfterSaveEvent event) {
+
+        System system = (System) event.getSource();
+        log.info("System with DatabaseID: '{}' has been persisted to MongoDB.",
+                system.getId());
+    }
+
+    @Override
+    public void onAfterDelete(AfterDeleteEvent event) {
+
+        if (event.getDocument() == null) return;
+
+        try {
+            log.info("System with DatabaseID: '{}' has been deleted from MongoDB.",
+                    (event.getDocument()).get("_id").toString());
+        } catch (NullPointerException e) {
+            log.error("Event reported as null during deletion.");
+        }
+    }
+
 }
